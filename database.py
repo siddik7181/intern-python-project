5c6a5ad85ade73492f4fcefc60587d54000be852
Nahid <Abu.Siddik@optimizely.com>
2024-12-16 14:42:02 +0600

    add sqlite database with uniquenesschecker contextmanager

---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+from contextlib import contextmanager
+
+@contextmanager
+def createdb(filename="mydb.db", table="tweets", schema="id INTEGER PRIMARY KEY AUTOINCREMENT, tweet TEXT"):
+    connection = sqlite3.connect(filename)
+    cursor = connection.cursor()
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table} ({schema})")
+    yield cursor
+    cursor.execute(f"DROP TABLE IF EXISTS {table}")
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.commit()
+        connection.close()
+
+class UniquenessChecker:
+    def __init__(self, cursor, table, column, column_value):
+        self.cursor = cursor
+        self.table = table
+        self.column = column
+        self.column_value = column_value
+
+    def __enter__(self):
+        query = f"SELECT 1 FROM {self.table} WHERE {self.column} = '{self.column_value}'"
+        self.cursor.execute(query)
+        res = self.cursor.fetchone()
+        if res:
+            raise ValueError('Duplicate value!!, value already exist in the database')
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+def addData(cursor, table, column, column_value):
+
+    with UniquenessChecker(cursor, table, column, column_value):
+        query = f"INSERT INTO {table} ({column}) VALUES ('{column_value}')"
+        cursor.execute(query, (column_value,))
+        print(f"Successfully added new row to {table}.")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from database import createdb, addData
 from getpass import getpass
 from User import User
 from pyjokes import get_joke
@@ -31,29 +32,39 @@ def main():
     tweets = (user.get_tweets()).get('body')
     user.show_tweets()
 
-    for tweet in tweets:
-        all_tweets.add(json.dumps(tweet))
+    with createdb() as cursor: 
+        for tweet in tweets:
+            print(f"Tweet Text: {tweet['text']}")
+            logger.debug(tweet['text'])
+            try:
+                addData(cursor, table="tweets", column="tweet", column_value=tweet['text'])
+            except ValueError as e:
+                print(e)
+                logger.error(e)
 
     new_tweets = 0
     while new_tweets < 10:
         tweet = get_joke()
-        if tweet not in all_tweets:
-            posted_tweet = (user.post_tweets(tweet)).get('body')
-            if isinstance(posted_tweet, dict):
-                all_tweets.add(json.dumps(posted_tweet))
+        print(tweets)
+        new_tweets += 1
+        continue
+        with createdb() as cursor: 
+            try:
+                addData(cursor, table="tweets", column="tweet", column_value=tweet)
+                posted_tweet = (user.post_tweets(tweet)).get('body')
                 new_tweets += 1
-                
                 if new_tweets < 10:
                     print('posted tweet. sleeping 1 min now.')
                     logger.debug('posted tweet. sleeping 1 min now.')
                 else:
                     print('posted tweet.')
                     logger.debug('posted tweet.')
-            else:
-                print("couldn't posted sorry, sleeping 1 min now ...")
-                logger.error("couldn't posted sorry, sleeping 1 min now ...")
-            sleep(1)
-    
+                sleep(1)
+
+            except ValueError as e:
+                print(e)
+                logger.error(e)
+
     print('10 new tweets posted ...')
     logger.info('10 new tweets posted ...')
 
